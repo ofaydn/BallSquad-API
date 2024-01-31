3019f7b84cb7ef9c3c64d15b428cba1e45139e7c
Ömer Faruk Aydın <o.faduzce@gmail.com>
2024-01-31 18:47:42 +0300

    upload basic files

---
+++ BallSquadApi/src/main/java/com/example/ballsquadapi/BallSquadApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ballsquadapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallSquadApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BallSquadApiApplication.class, args);
+    }
+
+}

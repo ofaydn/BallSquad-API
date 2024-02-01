2f723b76ccf3b92f0650ab1aac677ba3c9f7656d
Ömer Faruk Aydın <o.faduzce@gmail.com>
2024-02-01 18:24:05 +0300

    FeignClient dependency added
    This dependency will be used for making REST Calls

--- BallSquadApi/src/main/java/com/example/ballsquadapi/BallSquadApiApplication.java
+++ BallSquadApi/src/main/java/com/example/ballsquadapi/BallSquadApiApplication.java
@@ -2,8 +2,10 @@ package com.example.ballsquadapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BallSquadApiApplication {
 
     public static void main(String[] args) {
